9f177e22012e595b614ea7fbb35d09ee4ffde080
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-19 01:30:37 +0900

    Create Events.cs

---
+++ Assets/Scripts/Utils/Events.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Events;
+
+// Event を管理するクラス
+public class Events
+{
+    // GameManager の State が更新された時に発生する event (第一引数：変更した後の状態, 第二引数 : 変更する前の状態)
+    [System.Serializable] public class EventGamState : UnityEvent<GameManager.GameState, GameManager.GameState>{}
+}
