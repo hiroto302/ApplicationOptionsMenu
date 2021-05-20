a84249d7bd6a423b924c0f576959c064c9097d59
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-20 20:58:18 +0900

    言語切り替え - Create ScriptableObject TextData
    言語に対応した文字データを格納

---
+++ Assets/Scripts/ScriptableObject _Text/TextData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+// ScriptableObject : 言語に対応した文字のデータ
+[CreateAssetMenu(fileName = "New TextData", menuName = "TextData")]
+public class TextData : ScriptableObject
+{
+    public string EnglishText;  // 英語のテキスト
+    public string JapaneseText; // 日本語のテキスト
+}
