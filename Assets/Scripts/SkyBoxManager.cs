4c057a63c03854ab030a8c604b0c7f6313f6ceae
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-21 17:22:52 +0900

    Create SkyBOxManager
    シーンの sky 操作を管理するシステムクラス

---
+++ Assets/Scripts/SkyBoxManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SkyBox を制御するクラス
+public class SkyBoxManager : MonoSingleton<SkyBoxManager>
+{
+    [SerializeField] Material _eveningSky; // 夕空
+    [SerializeField] Material _nighSky;    // 夜空
+
+    void Start()
+    {
+        GameManager.Instance.OnGameStateChange.AddListener(HandleGameStateChange);
+    }
+
+    void HandleGameStateChange(GameManager.GameState currentState, GameManager.GameState previousState)
+    {
+        // StartScene => MainScen
+        if(previousState == GameManager.GameState.PREGAME && currentState == GameManager.GameState.RUNNING)
+        {
+            ChangeSkyBoxMaterial(_nighSky);
+        }
+
+        // MainScene => StartScene
+        if(previousState == GameManager.GameState.PAUSED && currentState == GameManager.GameState.PREGAME)
+        {
+            ChangeSkyBoxMaterial(_eveningSky);
+        }
+    }
+    void ChangeSkyBoxMaterial(Material skyMaterial)
+    {
+        RenderSettings.skybox = skyMaterial;
+    }
+
+}
