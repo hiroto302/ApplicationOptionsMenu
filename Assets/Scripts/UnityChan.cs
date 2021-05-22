a1f6b582db3a9865d740e4cecb2e51f11518687c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-05-22 12:36:08 +0900

    会話 - UnityChan に 触れた時、会話の吹き出し機能追加

---
+++ Assets/Scripts/UnityChan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// たまに出現する SD タイプの Unity-Chan 制御スクリプト
+public class UnityChan : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    [SerializeField] Canvas _unitychanCanvas;
+    void OnMouseDown()
+    {
+        // 触れた時, RaiseHand animation 開始
+        _animator.SetTrigger("HasTouched");
+    }
+
+    // RaiseHand animation の event で呼ばれるメソッド
+    void OnHandRaise()
+    {
+        // 会話表示
+        SetConversationPanelActive(true);
+    }
+
+    void OnHandDown()
+    {
+        // 会話非表示
+        SetConversationPanelActive(false);
+    }
+
+    // 会話する内容が記述されてるpanel
+    void SetConversationPanelActive(bool show)
+    {
+        _unitychanCanvas.gameObject.SetActive(show);
+    }
+}
